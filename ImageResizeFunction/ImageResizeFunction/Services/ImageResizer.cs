0999de4ba895647e8c95eb3445fd5f628488e4cf
Unzela Inamdar <51193316+unzela@users.noreply.github.com>
2022-05-02 00:14:01 +0530

    Add files via upload

---
+++ ImageResizeFunction/ImageResizeFunction/Services/ImageResizer.cs
@@ -0,0 +1,20 @@
+﻿using ImageResizeFunction.Services;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
+using System.IO;
+
+namespace ImageResizeFunction
+{
+    class ImageResizer : IImageResizer
+    {
+        public void Resize(Stream input, Stream output)
+        {
+            using (Image image = Image.Load(input))
+            {
+                image.Mutate(x => x.Resize(image.Width / 2, image.Height / 2));
+                image.Save(output, new JpegEncoder());
+            }
+        }
+    }
+}
